ca841ea0456c4929c30997193bdf7be4203bbd67
Baska Batbold <baskabatbold@gmail.com>
2018-07-26 08:46:01 -0500

    remove unused variable

--- src/test/java/gradle/cucumber/echoStepDefs.java
+++ src/test/java/gradle/cucumber/echoStepDefs.java
@@ -18,7 +18,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class echoStepDefs {
-  private Server server;
   private TestClient testClient;
   private Response response;
 
