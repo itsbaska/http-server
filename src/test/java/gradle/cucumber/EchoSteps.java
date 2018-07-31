83719cc22222303ba9ba8aaf45983d79a20d20d4
Baska Batbold <baskabatbold@gmail.com>
2018-07-31 10:34:46 -0500

    rename file and class

--- src/test/java/gradle/cucumber/EchoSteps.java
+++ src/test/java/gradle/cucumber/EchoSteps.java
@@ -21,29 +21,35 @@ import java.net.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class echoStepDefs {
+public class EchoSteps {
   private CloseableHttpClient httpclient;
   private CloseableHttpResponse response;
   private HttpGet httpGet;
-  private String responseBody;
   private HttpPost httpPost;
+  private static int DEFAULT_PORT = 3000;
+
+  private static boolean serverIsRunning(int port) throws IOException {
+    boolean result = false;
+    try {
+      (new Socket("127.0.0.1", port)).close();
+      result = true;
+    } catch (SocketException e) {
+      System.out.println(e);
+    }
+    return result;
+  }
 
   @Given("the server is running")
   public void serverIsRunning() throws IOException {
-    boolean result = false;
-    try {
-      (new Socket("127.0.0.1", 3000)).close();
-      result = true;
-    } catch(SocketException e) {
-      System.out.println(e);
-    }
-    assertTrue(result);
+    Runtime.getRuntime()
+    .exec("javac -cp src/main/java/StartServer.java && java -cp src/main/java StartServer -p " +  Integer.toString(DEFAULT_PORT));
+    assertTrue(serverIsRunning(DEFAULT_PORT));
   }
 
   @When("^I \"POST\" \"([^\"]*)\" to \"([^\"]*)\"$")
   public void iTo(String body, String path) throws Throwable {
     httpclient = HttpClients.createDefault();
-    httpPost = new HttpPost("http://localhost:3000" + path);
+    httpPost = new HttpPost("http://localhost:" + Integer.toString(DEFAULT_PORT) + path);
     response = httpclient.execute(httpPost);
     httpPost.setEntity(new StringEntity(body));
     CloseableHttpResponse response = httpclient.execute(httpPost);
@@ -58,12 +64,13 @@ public class echoStepDefs {
   @When("^I request \"GET\" \"([^\"]*)\"$")
   public void iRequest(String path) throws Throwable {
     httpclient = HttpClients.createDefault();
-    httpGet = new HttpGet("http://localhost:3000" + path);
+    httpGet = new HttpGet("http://localhost:" + Integer.toString(DEFAULT_PORT) + path);
     response = httpclient.execute(httpGet);
   }
 
   @And("the response body should be empty")
   public void the_response_body_should_be_empty() throws IOException {
+    String responseBody;
     try {
       HttpEntity entity = response.getEntity();
       EntityUtils.consume(entity);
@@ -92,5 +99,24 @@ public class echoStepDefs {
       response.close();
     }
     assertEquals(responseBody, responseBody1);
+  }
+
+  @Given("^I am in a console shell$")
+  public void iAmInAConsoleShell() throws Throwable {
+    Runtime.getRuntime()
+    .exec("javac -cp src/main/java/StartServer.java ");
+  }
+
+  @When("^I start the server with the option \"([^\"]*)\"$")
+  public void iStartTheServerWithTheOption(String option) throws Throwable {
+    Runtime.getRuntime()
+    .exec("java -cp src/main/java StartServer " + option);
+  }
+
+  @And("^I request \"GET\" \"([^\"]*)\" on port \"([^\"]*)\"$")
+  public void iRequestOnPort(String path, String port) throws Throwable {
+    httpclient = HttpClients.createDefault();
+    httpGet = new HttpGet("http://127.0.0.1:" + port + path);
+    response = httpclient.execute(httpGet);
   }
 }
