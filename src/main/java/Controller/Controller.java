8c4c6683968a5778e3cc08e63181bec481965657
Baska Batbold <baskabatbold@gmail.com>
2018-08-09 09:47:18 -0500

    wip

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,5 +1,4 @@
 package Controller;
-import Config.Handler.GetHandler;
 import Config.Handler.HandlerFactory;
 import Config.Handler.RequestHandler;
 import Request.Request;
@@ -12,37 +11,37 @@ public class Controller {
       .setbody("I am this Response2")
       .build();
     RequestHandler handler = new HandlerFactory().createHandler(request);
-    return handler.handleRequest(request, response2);
-//
-//    Response response = null;
-//    switch (request.path) {
-//      case "/":
-//        switch (request.method) {
-//          case GET:
-//            response = new Response.Builder()
-//              .setStatusCode(200)
-//              .setbody("")
-//              .build();
-//            break;
-//        }
-//        break;
-//      case "/echo":
-//        switch (request.method) {
-//          case GET:
-//            response = new Response.Builder()
-//              .setStatusCode(200)
-//              .setbody("")
-//              .build();
-//            break;
-//          case POST:
-//            response = new Response.Builder()
-//              .setStatusCode(200)
-//              .setbody(request.body)
-//              .build();
-//            break;
-//        }
-//        break;
-//    }
-//    return response;
+    handler.handleRequest(request, response2); // this returns a response object
+
+    Response response = null;
+    switch (request.path) {
+      case "/":
+        switch (request.method) {
+          case GET:
+            response = new Response.Builder()
+              .setStatusCode(200)
+              .setbody("")
+              .build();
+            break;
+        }
+        break;
+      case "/echo":
+        switch (request.method) {
+          case GET:
+            response = new Response.Builder()
+              .setStatusCode(200)
+              .setbody("")
+              .build();
+            break;
+          case POST:
+            response = new Response.Builder()
+              .setStatusCode(200)
+              .setbody(request.body)
+              .build();
+            break;
+        }
+        break;
+    }
+    return response;
   }
 }
